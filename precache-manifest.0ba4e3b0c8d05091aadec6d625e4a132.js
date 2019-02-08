b5f089bcc0c4affe81b556f83255f235a120f1de
Flavor85 <flaviengr75@gmail.com>
2019-02-08 09:24:32 +0100

    Updates

---
+++ precache-manifest.0ba4e3b0c8d05091aadec6d625e4a132.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eddb3aa8553140b81695",
+    "url": "/js-calculator/static/js/runtime~main.eddb3aa8.js"
+  },
+  {
+    "revision": "8eb5d6c1287f14ee898f",
+    "url": "/js-calculator/static/js/main.8eb5d6c1.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/js-calculator/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "8eb5d6c1287f14ee898f",
+    "url": "/js-calculator/static/css/main.362e9086.chunk.css"
+  },
+  {
+    "revision": "de8a251a5d30b0180b1132a96b765c47",
+    "url": "/js-calculator/index.html"
+  }
+];
